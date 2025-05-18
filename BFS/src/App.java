4ae8c92f4abf1838a94c445d4ef8660d5b367d60
nurealamriyal <nurealamriyal@gmail.com>
2025-05-19 00:18:19 +0600

    bfs,dfs,dijktra add

---
+++ BFS/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+   
+		public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Graph graph = new Graph(7);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 5);
+        graph.addEdge(2, 6);
+
+        System.out.println("Breadth-First Search (BFS) traversal starting from vertex 0:");
+        graph.BFS(0);}
+}
